9a73cc2ffab9c29307e3ee29ccd8caf54e8fc0a2
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-14 21:21:49 -0700

    new util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,3 +14,39 @@ export function slugify(text: string) {
       timeZone: "UTC",
     })
   }
+
+  export function formatBlogPosts(posts, {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}) {
+  
+    const filteredPosts = posts.reduce((acc, post) => {
+      const { date, draft } = post.frontmatter;
+      // filterOutDrafts if true
+      if (filterOutDrafts && draft) return acc;
+  
+      // filterOutFuturePosts if true
+      if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+  
+      // add post to acc
+      acc.push(post)
+  
+      return acc;
+    }, [])
+  
+    // sortByDate or randomize
+    if (sortByDate) {
+      filteredPosts.sort((a, b) => new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime())
+    } else {
+      filteredPosts.sort(() => Math.random() - 0.5)
+    }
+  
+    // limit if number is passed
+    if (typeof limit === "number") {
+      return filteredPosts.slice(0, limit);
+    }
+    return filteredPosts;
+  
+  }
