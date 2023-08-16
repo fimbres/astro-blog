339ae5631280700bb61d6925f64bead12badbf6e
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-15 18:36:20 -0700

    adding tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), react()]
+  }), sitemap(), react(), tailwind()]
 });
