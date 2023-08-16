04c787b7a3c3880b04b97791855989960c3ff807
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-15 17:56:58 -0700

    setting up sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import image from "@astrojs/image";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    sitemap()
+  ]
 });
